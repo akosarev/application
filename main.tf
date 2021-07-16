4dd5be2a86196b5486f78794aa0fe3509d5a9497
Artemiy Kosarev <artemiy.kosarev@klarna.com>
2021-07-16 10:42:53 +0200

    Test deploy

--- main.tf
+++ main.tf
@@ -15,9 +15,6 @@ provider "aws" {
   region     = "eu-west-1"
 }
 
-provider "tls" {
-
-}
 data "aws_eks_cluster" "cluster" {
   name = "eks_infra"
 }
@@ -30,6 +27,5 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-
 }
 
